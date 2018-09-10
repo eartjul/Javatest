ee1728aed4134d8d13cbf8a368128a6270ab269c
eartjul <julia.artiukhova@gmail.com>
2018-09-10 19:19:09 +0300

    initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String [] args) {
+        String out = "Hello world!!!";
+        int i = 1;
+        System.out.println(out + 1);
+    }
+}
